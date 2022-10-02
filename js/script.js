31eea3975ec2228af6b568da004d7b2103ccb386
Sakhe Zikolo <zikolo.sakhe@gmail.com>
2022-10-02 15:50:17 +0200

    :construction: added window event listener and the canvas with width and height :construction:

---
+++ js/script.js
@@ -0,0 +1,42 @@
+window.addEventListener("load", function() {
+    // canvas setup
+    const canvas = documentgetElementById("canvas1");
+    const ctx = canvas.getContext("2d");
+    canvas.width = 1500;
+    canvas.height = 500;
+
+    class InputHandler {
+
+    }
+
+    class Projectile {
+
+    }
+
+    class Particle {
+
+    }
+
+    class Player {
+    }
+
+    class Enemy {
+
+    }
+
+    class Layer {
+
+    }
+
+    class Background {
+
+    }
+
+    class UI {
+
+    }
+
+    class Game {
+
+    }
+});
