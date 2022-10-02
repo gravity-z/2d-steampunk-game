e5cf0fd441f12311ffff0b6690b1442b5436c8d7
Sakhe Zikolo <zikolo.sakhe@gmail.com>
2022-10-02 16:09:30 +0200

    :construction: Added yellow color to the projectiles in draw method :construction:

--- js/script.js
+++ js/script.js
@@ -37,6 +37,18 @@ window.addEventListener("load", function() {
             this.speed = 3;
             this.markedForDeletion = false;
         }
+
+        update() {
+            this.x += this.speed;
+            if (this.x > this.game.width * 0.8) {
+                this.markedForDeletion = true;
+            }
+        }
+
+        draw(context) {
+            context.fillStyle = 'yellow';
+            fillRect(this.x, this.y, this.width, this.height);
+        }
     }
 
     class Particle {
