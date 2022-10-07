8ff12bb4efc544828c5f87024bb00473023af759
Sakhe Zikolo <zikolo.sakhe@gmail.com>
2022-10-07 19:32:25 +0200

    :construction: Added score. USE AS BASE FOR OTHER GAMES

--- js/script.js
+++ js/script.js
@@ -154,16 +154,25 @@ window.addEventListener("load", function() {
             this.game = game;
             this.fontSize = 25;
             this.fontFamily = 'Helvetica';
-            this.color = 'yellow';
+            this.color = 'white';
         }
 
         draw(context) {
+            context.save();
+            context.fillStyle = this.color;
+            context.shadowOffSetX = 2;
+            context.shadowOffSetY = 2;
+            context.shadowColor = 'black';
+            context.font = this.fontSize + 'px ' + this.fontFamily;
+            //score
+            context.fillText('Score: ' + this.game.score, 20, 40);
             // ammo
             context.fillStyle = this.color;
             
             for (let i = 0; i < this.game.ammo; i++) {
                 context.fillRect(20 + 5 * i, 50, 3, 20);
             }
+            context.restore();
         }
 
     }
@@ -184,6 +193,8 @@ window.addEventListener("load", function() {
             this.ammoTimer = 0;
             this.ammoInterval = 500;
             this.gameOver = false;
+            this.score = 0;
+            this.winningScore = 10;
         }
 
         update(deltaTime) {
@@ -210,6 +221,9 @@ window.addEventListener("load", function() {
                         if (enemy.lives <= 0) {
                             enemy.markedForDeletion = true;
                             this.score += enemy.score;
+                            if (this.score > this.winningScore) {
+                                this.gameOver = true;
+                            }
                         }
                     }
                 });
